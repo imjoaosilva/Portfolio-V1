82f37ccf0ff23b42922cf22cd8506aed1733970a
Z4 <z4kreyyt@gmail.com>
2026-04-02 00:45:27 +0100

    fix: Github Contributions getting 2025 instead of 2026

--- src/features/portfolio/data/github-contributions.ts
+++ src/features/portfolio/data/github-contributions.ts
@@ -7,10 +7,14 @@ type GitHubContributionsResponse = {
 	contributions: Activity[];
 };
 
-export const getGitHubContributions = unstable_cache(async () => {
-	const res = await fetch(
-		`https://github-contributions-api.jogruber.de/v4/${USER_DATA.github_username}?y=last`,
-	);
-	const data = (await res.json()) as GitHubContributionsResponse;
-	return data.contributions;
-}, ["github-contributions"], { revalidate: 86400 }); // Cache for 1 day (86400 seconds)
+export const getGitHubContributions = unstable_cache(
+	async () => {
+		const res = await fetch(
+			`https://github-contributions-api.jogruber.de/v4/${USER_DATA.github_username}?y=2026`,
+		);
+		const data = (await res.json()) as GitHubContributionsResponse;
+		return data.contributions;
+	},
+	["github-contributions"],
+	{ revalidate: 86400 },
+); // Cache for 1 day (86400 seconds)
